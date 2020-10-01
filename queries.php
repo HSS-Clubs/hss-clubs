d95102a554a0f5bca37671330b1df9b917212319
JaiNarayanan <47904405+JaiNarayanan@users.noreply.github.com>
2020-10-01 14:54:57 -0400

    Add files via upload

--- queries.php
+++ queries.php
@@ -1,25 +1,216 @@
 <?php
-
-$conn = mysqli_connect("localhost:3306","root","","virtualfair");
+$conn = mysqli_connect("localhost:3306", "root", "", "virtualfair");
 // $recieve = json_decode(file_get_contents('php://input'), true);
 // $state = $recieve["state"];
 // $state = $_POST["state"];
-$result = mysqli_query($conn, "SELECT * FROM clubs");
+$type = $_POST['type'];
 
-$data = array();
-while($row = mysqli_fetch_assoc($result))
-{
-    $event = (object) [
-        'name' => $row["cname"],
-        'bio' => $row["cbio"],
-        'link' => $row["clink"],
-        'advisor' => $row["cadvisor"],
-        'website' => $row["website"]
-        
-    ];
-    array_push($data ,$event);
+$parameters = $_POST['parameters'];
+if (isset($parameters["idverify"])){
+    $idtoken = $parameters["idverify"];
+    $verifyUrl = "https://oauth2.googleapis.com/tokeninfo?id_token=" . substr($idtoken, 1, strlen($idtoken) - 1);  
+        $json = file_get_contents($verifyUrl);
+    if ($json !== false) {
+        $obj = json_decode($json);
+        $verifiedEmail = $obj->email_verified;
+}  
 }
 
-echo json_encode($data);
 
-?>
+
+if($type=="clubLogIn"){
+    $email = $parameters["email"];
+    $password = $parameters["password"];
+    $stmt = $conn->prepare("SELECT clubID, cpassword FROM clubs WHERE cemail = ?");
+    $stmt->bind_param("s",$parameters["email"]);    
+    $stmt->execute();
+    $result = $stmt->get_result();
+    // $result = mysqli_query($conn, "SELECT clubID, cpassword FROM clubs WHERE cemail = '" . $parameters["email"] . "'");
+    while($row = $result->fetch_assoc()) {
+        $passwordValid = $row['cpassword'];
+        $clubId = $row["clubID"];
+      }
+    if($passwordValid==$password){
+        echo "valid".$clubId;   
+    }
+    else{
+        echo "INVALID BUDDY";
+    }
+}
+else if($type=="getMemberList"){
+    $clubID = $parameters["clubId"];
+    $stmt = $conn->prepare("SELECT students.name,students.email FROM students INNER JOIN registrations ON students.studentID=registrations.studentID WHERE registrations.clubID=?");
+    $stmt->bind_param("i",$clubID);    
+    $stmt->execute();
+    $result = $stmt->get_result();
+    // $result = mysqli_query($conn, "SELECT students.name,students.email FROM students INNER JOIN registrations ON students.studentID=registrations.studentID WHERE registrations.clubID='".$clubID."'");
+    $data = array();
+    while($row = $result->fetch_assoc()) {
+        $event = (object) [
+        'name' => $row["name"],
+        'email' => $row["email"]
+        ];
+        array_push($data, $event);
+    }
+    echo json_encode($data);
+}
+else if($type=="getClubDetails"){
+    $clubID = $parameters["clubId"];
+    $stmt = $conn->prepare("SELECT * FROM clubs WHERE clubID = ?");
+    $stmt->bind_param("i",$clubID);    
+    $stmt->execute();
+    $result = $stmt->get_result();
+    // $result = mysqli_query($conn, "SELECT * FROM clubs WHERE clubID = '" . $clubID . "'");
+    $data = array();
+    while($row = $result->fetch_assoc()) {
+        $event = (object) [
+        'name' => $row["cname"],
+        'genre' => $row["cgenre"],
+        'vhours' => $row["volunteering"],
+        'advisor' => $row["cadvisor"],
+        'bio' => $row["cbio"],
+        'officers' => $row["cofficers"],
+        'website' => $row["website"],
+        'classroom' => $row["ccode"],
+        ];
+        array_push($data, $event);
+    }
+    echo json_encode($data);
+}
+else if($type=="updateClub"){
+    $clubID = $parameters["clubId"];
+    $name = $parameters["formData"]["name"];
+    $genre = $parameters["formData"]["genre"];
+    $advisor = $parameters["formData"]["advisor"];
+    $volunteer = $parameters["formData"]["volunteer"];
+    $bio = $parameters["formData"]["bio"];
+    $officers = $parameters["formData"]["officers"];
+    $website = $parameters["formData"]["website"];
+    $club = $parameters["formData"]["code"];
+    $stmt = $conn->prepare("UPDATE clubs SET cname = ?, cbio= ?, ccode= ?, cofficers= ?, cadvisor= ?,
+     cgenre= ?, volunteering= ?, website= ? WHERE clubID = ?");
+    $stmt->bind_param("ssssssssi",$name,$bio,$club,$officers,$advisor,$genre,$volunteer,$website,$clubID);    
+    $stmt->execute();
+    $result = $stmt->get_result();    
+    // mysqli_query($conn, "UPDATE clubs SET cname = '". $name . "', cbio= '". $bio . "', ccode= '". $club . "', cofficers= '". $officers . "', cadvisor= '". $advisor . "', cgenre= '". $genre . "', volunteering= '". $volunteer . "', website= '". $website . "' WHERE clubID = '".$clubID."';");
+    
+
+}
+//Student Page
+else if ($verifiedEmail == true) {
+    if ($type == "allClubs") {
+        // "SELECT * from clubs WHERE clubID not in ( SELECT clubID from registrations WHERE studentID='" . $parameters["userID"] . "')"
+        $stmt = $conn->prepare("SELECT *,'y' isMember from clubs WHERE clubID in ( SELECT clubID from registrations WHERE studentID=?)
+        UNION ALL SELECT *,'n' isMember from clubs WHERE clubID not in ( SELECT clubID from registrations WHERE studentID=?)");
+        $stmt->bind_param("ii",$parameters["userID"],$parameters["userID"]);    
+        $stmt->execute();
+        $result = $stmt->get_result();
+        // $result = mysqli_query($conn, "SELECT *,'y' isMember from clubs WHERE clubID in ( SELECT clubID from registrations WHERE studentID='" . $parameters["userID"] . "')
+        // UNION ALL SELECT *,'n' isMember from clubs WHERE clubID not in ( SELECT clubID from registrations WHERE studentID='" . $parameters["userID"] . "')" );
+        $data = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+            $event = (object) [
+                'id' => $row["clubID"],
+                'name' => $row["cname"],
+                'bio' => $row["cbio"],
+                'volunteer' => $row["volunteering"],
+                'link' => $row["ccode"],
+                'advisor' => $row["cadvisor"],
+                'email' => $row["cemail"],
+                'genre' => $row["cgenre"],
+                'website' => $row["website"],
+                'isMember' => $row["isMember"]
+            ];
+            array_push($data, $event);
+        }
+
+        echo json_encode($data);
+    }
+    else if($type=="getUserClubs"){
+        $stmt = $conn->prepare("SELECT * from clubs WHERE clubID in ( SELECT clubID from registrations WHERE studentID=?)");
+        $stmt->bind_param("i",$parameters["userID"]);    
+        $stmt->execute();
+        $result = $stmt->get_result();
+        // $result = mysqli_query($conn, "SELECT * from clubs WHERE clubID in ( SELECT clubID from registrations WHERE studentID='" . $parameters["userID"] . "')");
+        $data = array();
+        if ($result->num_rows == 0) {
+            echo ("empty");
+        }   
+        else{
+        while ($row = mysqli_fetch_assoc($result)) {
+            $event = (object) [
+                'id' => $row["clubID"],
+                'name' => $row["cname"],
+                'bio' => $row["cbio"],
+                'volunteer' => $row["volunteering"],
+                'link' => $row["ccode"],
+                'advisor' => $row["cadvisor"],
+                'email' => $row["cemail"],
+                'genre' => $row["cgenre"],
+                'website' => $row["website"]
+            ];
+            array_push($data, $event);
+        }
+
+        echo json_encode($data);
+        }
+    } 
+    else if ($type == "userLogIn") {
+        $data = array();
+        $stmt = $conn->prepare("SELECT * FROM students WHERE email = ?");
+        $stmt->bind_param("s",$parameters["email"]);    
+        $stmt->execute();
+        $result = $stmt->get_result();
+        // $result = mysqli_query($conn, "SELECT * FROM students WHERE email = '" . $parameters["email"] . "'");
+
+        if ($result->num_rows == 1) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $event = (object) [
+                    'id' => $row["studentID"],
+                ];
+                array_push($data, $event);
+                echo json_encode($data);
+            }
+        } else {
+            $stmt = $conn->prepare("INSERT INTO students (name, email) VALUES (?,?)");
+            $stmt->bind_param("ss",$parameters["name"],$parameters["email"]);    
+            $stmt->execute();
+            $result2 = $stmt->get_result();
+            // $result2 = mysqli_query($conn, "INSERT INTO students (name, email) VALUES ('" . $parameters["name"] . "','" . $parameters["email"] . "')");
+            $stmt = $conn->prepare("SELECT * FROM students WHERE email =?");
+            $stmt->bind_param("s",$parameters["email"]);    
+            $stmt->execute();
+            $result3 = $stmt->get_result();
+            // $result3 = mysqli_query($conn, "SELECT * FROM students WHERE email = '" . $parameters["email"] . "'");
+                while ($row = mysqli_fetch_assoc($result3)) {
+                    $event = (object) [
+                        'id' => $row["studentID"],
+                    ];
+                    array_push($data, $event);                        
+                }
+                echo json_encode($data);
+        }
+    }
+    else if($type == "userJoinClub"){
+        $stmt = $conn->prepare("INSERT INTO registrations (studentID, clubID) VALUES (?,?)");
+        $stmt->bind_param("ii",$parameters["userID"],$parameters["clubID"]);    
+        $stmt->execute();
+        $result = $stmt->get_result();
+        // $result = mysqli_query($conn, "INSERT INTO registrations (studentID, clubID) VALUES ('" . $parameters["userID"] . "','" . $parameters["clubID"] . "')");
+    }
+    else if($type == "userLeaveClub"){
+        $stmt = $conn->prepare("DELETE FROM registrations WHERE studentID = ? AND clubID=?");
+        $stmt->bind_param("ii",$parameters["userID"],$parameters["clubID"]);    
+        $stmt->execute();
+        $result = $stmt->get_result();
+        // $result = mysqli_query($conn, "DELETE FROM registrations WHERE studentID = '" . $parameters["userID"] . "' AND clubID='" . $parameters["clubID"] . "'");
+    }
+    
+     else if ($type == "addClub") {
+        echo "addCLub called";
+        // echo ($parameters);
+    }
+    
+} else {
+    echo ("invalid login");
+}
--- query.js
+++ query.js
@@ -1,46 +1,386 @@
 function getAllClubs(){
+	var dataSet = {userID:sessionStorage.getItem('userID'),idverify:sessionStorage.getItem('verifyid')}	
 $.ajax({
 	url: "queries.php",
 	cache: false,
 	type: "POST",
+	data: {type:"allClubs",parameters:dataSet},
 	success: function(res) {
+		if(res.substring(res.length-13)=="invalid login"){
+			sessionInvalid()
+		}
 		var data = JSON.parse(res);
+		// if(res="")
+		// console.log(data)
 		listContent = ``
 		for (i = 0; i < data.length; i++) {
 			colors=["#BAD4AA","#EDB458"];
 			var name = data[i]["name"];            
-			console.log(data[i])
-			console.log(name)
+
 			var bio = data[i]["bio"];
+			var id = data[i]["id"];
 			var link = data[i]["link"];
 			var advisor = data[i]["advisor"];
+			var volunteer = data[i]["volunteer"];
+			var genre = data[i]["genre"];
 			var website = data[i]["website"];	
-			color=colors[i];
-			listContent +=`<div class="course">
-			  <div style="background-color:${color}"class="preview square d-flex flex-column align-items-center">
-			  <h6>Humanities</h6>
-			  <h2 style="text-align:center">${name}</h2>
-			  <div class="row">
-				  <!-- <div class="col-sm-2"></div> -->
-				  <div class="col-sm-12">
-					  <img class="rounded-circle img-fluid" alt="100x100" src="imgs/Debate.png"
-					  data-holder-rendered="true">
+			var email = data[i]["email"];
+			var isMember = data[i]["isMember"];
+			var joinButton = "JOIN";
+			var disabledButton = "";
+			if(isMember == "y"){
+				joinButton="JOINED"
+				disabledButton = "disabled";
+			}
+			color=colors[(parseInt(id)+2)%2];
+			// console.log(typeof parseInt(id))
+			if(volunteer=="true"){
+				volunteer="checked";
+			}
+			else{
+				volunteer="unchecked";
+			}
+			if(advisor=="true"){
+				advisor="checked";
+			}
+			else{
+				advisor="unchecked";
+			}
+			listContent +=`<div class="container tab-pane  active pt-5" id="nav-bio" role="tabpanel" aria-labelledby="nav-bio-tab" style="text-align:left;">
+			<div class="card" style="border-radius:5x;">
+			  <div class="square card-header border-0" style="text-align:center;background-color:${color} ;color:white;">
+				  <h6 style="text-align:center;text-transform:uppercase;">${genre}</h6>
+				  <div style="text-align:center;"><h2 style="text-align:center;">${name}</h2></div>
+				  <div class="row">
+					  <div class="col-sm-12">
+						  <img class="rounded-circle img-fluid" alt="100x100" src="imgs/debate.png" height="120vh" width="120vw"
+						  data-holder-rendered="true">
+					  </div>
 				  </div>
-				  <!-- <div class="col-sm-2"></div> -->
 			  </div>
+			  <div class="card-body px-2" style="flex-wrap:wrap;">
+				  <div class="container">
+					<div class="row">
+					  <div class="col-sm-9">
+						<p class="card-text" style="">praesentium libero, quae quas, excepturi neque quaerat atque, illo eaque sed cum nisi ducimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis officiis dolore odio unde et voluptate blanditiis praesentium libero, quae quas, excepturi neque quaerat atque, illo eaque sed cum nisi ducimus.</p>
+					<a href="javascript:void(0)"onclick="userJoinClub(${id})"  class="btn ${disabledButton} cardBtn btn-primary pull-left content-desktop">${joinButton}</a>
+					  </div>
+					  <div class="col-sm-3 d-flex d-row flex-column align-items-center justify-content-left" style="text-align:left;">
+						<h1 style="font-size:13px;">Club E-mail: <a style="font-size:13px;"href="mailto:${email}">${email}</a> </h1>
+						<p style="margin-bottom:0;"><strong>Classroom Code: </strong>${link}</p>
+						<div>
+						  <input class="form-check-input" type="checkbox" ${advisor} disabled value="" id="defaultCheck1">
+						  <label class="form-check-label" for="defaultCheck1">
+							Advisor Run
+						  </label>
+						</div>
+						<div>
+						  <input class="form-check-input" ${volunteer} disabled type="checkbox" value="" id="defaultCheck2">
+						  <label class="form-check-label" for="defaultCheck2">
+							Community Service
+						  </label>
+						</div>
+						<a href="javascript:void(0)" onclick="userJoinClub(${id})"  class="btn ${disabledButton} cardBtn btn-primary pull-left content-mobile" style="text-align:center;margin-top:10px;" >${joinButton}</a>
+					  </div>
+					</div>
+				  </div>
 			  </div>
-			  <div class="info">
-			  
-			  <h2>${name}</h2>
-			  <p class="p-trunc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis officiis dolore odio unde et voluptate blanditiis praesentium libero, quae quas, excepturi neque quaerat atque, illo eaque sed cum nisi ducimus.</p>
-			  <button style="outline:none;"class="cardButton">Learn More</button>
-			  </div>
+		  </div>
+		  
 		  </div>`
 		}
 		// newContent+=`</tbody></table>`;
-		document.getElementById("all_clubs").innerHTML = listContent;
+
+		document.getElementById("nav-tabContent").innerHTML = listContent;
 		// document.getElementById("main-content").innerHTML = newContent;
 	}
-	
 });
+}
+function userClubs(){
+	var dataSet = {userID:sessionStorage.getItem('userID'),idverify:sessionStorage.getItem('verifyid')}	
+	// console.log(dataSet)
+	$.ajax({
+		url: "queries.php",
+		cache: false,
+		type: "POST",
+		data: {type:"getUserClubs",parameters:dataSet},
+		success: function(res) {
+			// console.log(res)
+			if(res.substring(res.length-13)=="invalid login"){
+				sessionInvalid()
+			}
+			listContent = ``
+			if(res=="empty"){
+				console.log("no vals")
+				listContent+=`<h1 class="emptyTab" >Go to The All Clubs Tab to Start Joining Clubs!</h1>`
+			}
+			else{
+			var data = JSON.parse(res);
+			for (i = 0; i < data.length; i++) {
+				colors=["#BAD4AA","#EDB458"];
+				var name = data[i]["name"];            
+				// console.log(data[i])
+				// console.log(name)
+				var bio = data[i]["bio"];
+				var id = data[i]["id"];
+				var link = data[i]["link"];
+				var advisor = data[i]["advisor"];
+				var volunteer = data[i]["volunteer"];
+				var genre = data[i]["genre"];
+				var website = data[i]["website"];	
+				var email = data[i]["email"];
+
+				color=colors[(parseInt(id)+2)%2];
+				if(volunteer=="true"){
+					volunteer="checked";
+				}
+				else{
+					volunteer="unchecked";
+				}
+				if(advisor=="true"){
+					advisor="checked";
+				}
+				else{
+					advisor="unchecked";
+				}
+				// console.log(advisor)
+	
+				// console.log(volunteer)
+				// ${color}
+				listContent +=`
+				<div class="container tab-pane  active pt-5" id="nav-bio" role="tabpanel" aria-labelledby="nav-bio-tab" style="text-align:left;">
+				<div class="card" style="border-radius:5x;">
+				  <div class="square card-header border-0" style="text-align:center;background-color:${color} ;color:white;">
+					  <h6 style="text-align:center;text-transform:uppercase;">${genre}</h6>
+					  <div style="text-align:center;"><h2 style="text-align:center;">${name}</h2></div>
+					  <div class="row">
+						  <div class="col-sm-12">
+							  <img class="rounded-circle img-fluid" alt="100x100" src="imgs/debate.png" height="120vh" width="120vw"
+							  data-holder-rendered="true">
+						  </div>
+					  </div>
+				  </div>
+				  <div class="card-body px-2" style="flex-wrap:wrap;">
+					  <div class="container">
+						<div class="row">
+						  <div class="col-sm-9">
+							<p class="card-text" style="">praesentium libero, quae quas, excepturi neque quaerat atque, illo eaque sed cum nisi ducimus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis officiis dolore odio unde et voluptate blanditiis praesentium libero, quae quas, excepturi neque quaerat atque, illo eaque sed cum nisi ducimus.</p>
+						<a href="javascript:void(0)" onclick="userLeaveClub(${id})"class="btn cardBtn btn-danger pull-left content-desktop">LEAVE</a>
+						  </div>
+						  <div class="col-sm-3 d-flex d-row flex-column align-items-center justify-content-left" style="text-align:left;">
+							<h1 style="font-size:13px;">Club E-mail: <a style="font-size:13px;"href="mailto:${email}">${email}</a> </h1>
+							<p style="margin-bottom:0;"><strong>Classroom Code: </strong>${link}</p>
+							<div>
+							  <input class="form-check-input" type="checkbox" ${advisor} disabled value="" id="defaultCheck1">
+							  <label class="form-check-label" for="defaultCheck1">
+								Advisor Run
+							  </label>
+							</div>
+							<div>
+							  <input class="form-check-input" ${volunteer} disabled type="checkbox" value="" id="defaultCheck2">
+							  <label class="form-check-label" for="defaultCheck2">
+								Community Service
+							  </label>
+							</div>
+							<a href="javascript:void(0)" onclick="userLeaveClub(${id})"class="btn cardBtn btn-danger pull-left content-mobile" style="text-align:center;margin-top:10px;">LEAVE</a>
+						  </div>
+						</div>
+					  </div>
+				  </div>
+			  </div>
+			  
+			  </div>`
+			}
+		}
+			// newContent+=`</tbody></table>`;
+			// 
+			document.getElementById("nav-tabContent").innerHTML = listContent;
+			// document.getElementById("main-content").innerHTML = newContent;
+		}
+	});
+}
+function userSignIn(nameVal, emailVal){
+	var dataSet = {name: nameVal, email: emailVal, idverify:sessionStorage.getItem('verifyid')}
+	$.ajax({
+		url: "queries.php",
+		cache: false,
+		type: "POST",
+		data: {type:"userLogIn",parameters:dataSet},
+		success: function(res) {
+			// console.log(typeof res)
+			var data = JSON.parse(res)
+			var id = parseInt(data[0]["id"])
+		
+			sessionStorage.setItem('userID',id);
+			loggedIn()
+		}
+	}); 
+}
+function addClub(idVal){
+	var dataSet = {id: idVal, idverify:sessionStorage.getItem('verifyid')}
+	$.ajax({
+		url: "queries.php",
+		cache: false,
+		type: "POST",
+		data: {type:"addClub",parameters:dataSet},
+		success: function(res) {
+			// console.log(res)
+		}
+	});
+}
+
+function clubLogIn(){
+	var emailVal = document.getElementById("inputEmail").value
+	var passwordVal = document.getElementById("inputPassword").value
+	var dataSet = {email: emailVal, password:passwordVal}
+	$.ajax({
+		url: "queries.php",
+		cache: false,
+		type: "POST",
+		data: {type:"clubLogIn",parameters:dataSet},
+		success: function(res) {
+			sessionStorage.setItem("clubTempID", "logidlolnooneknwos");
+			if(res.substring(0,5)=="valid"){
+				// console.log(res.substring(5))
+				getMembers(res.substring(5))
+				clubDetails(res.substring(5))
+				memberList()
+			}
+			else{
+				// console.log(res)
+			}
+		}
+	});
+}
+function getMembers(id){
+	// console.log(parseInt(id))
+	var dataSet = {clubId: id}
+	$.ajax({
+		url: "queries.php",
+		cache: false,
+		type: "POST",
+		data: {type:"getMemberList",parameters:dataSet},
+		success: function(res) {
+			// console.log(res)
+			var data = JSON.parse(res);
+			var memberArray = [];
+			for (i = 0; i < data.length; i++) {
+				var memberSpecific = [];
+				var name = data[i]["name"];            
+				var email = data[i]["email"];
+				memberSpecific.push(name);
+				memberSpecific.push(email);
+				// console.log(memberSpecific)
+				memberArray.push(memberSpecific);          				
+			}
+			// console.log(memberArray);
+			datatable.clear().draw();
+			datatable.rows.add(memberArray);
+			datatable.columns.adjust().draw();
+		}
+	});
+}
+function clubDetails(id){
+	var dataSet = {clubId: id}
+	$.ajax({
+		url: "queries.php",
+		cache: false,
+		type: "POST",
+		data: {type:"getClubDetails",parameters:dataSet},
+		success: function(res) {
+			var obj = JSON.parse(res)
+			console.log(obj)
+			document.getElementById("inputName").value = obj[0]["name"];
+			document.getElementById("inputGenre").value = obj[0]["genre"];
+			if(obj[0]["advisor"]!="false"){
+				document.getElementById("gridCheck2").checked = true;;
+			}
+			else{
+				document.getElementById("gridCheck2").checked = false;
+			}
+			if(obj[0]["vhours"]!="false"){
+				document.getElementById("gridCheck").checked = true;;
+			}
+			else{
+				document.getElementById("gridCheck").checked = false;
+			}
+			document.getElementById("ClubBio").value = obj[0]["bio"];
+			document.getElementById("ClubOfficers").value = obj[0]["officers"];
+			document.getElementById("ClubWebsite").value = obj[0]["website"];
+			document.getElementById("ClubCode").value = obj[0]["classroom"];
+			document.getElementById("saveButton").value= id;
+		}
+	});
+}
+function updateClub(){
+	if(document.getElementById("saveButton").value!="notAssigned"){
+	var nameVal = document.getElementById("inputName").value;
+	var genreVal = document.getElementById("inputGenre").value;
+	var advisorVal = document.getElementById("gridCheck2").checked;
+	var volunteerVal = document.getElementById("gridCheck").checked;
+	var clubBio = document.getElementById("ClubBio").value ;
+	var clubOfficers = document.getElementById("ClubOfficers").value;
+	var clubWebsite = document.getElementById("ClubWebsite").value;
+	var clubCode = document.getElementById("ClubCode").value;
+
+	var formdata = {name: nameVal, genre: genreVal, advisor:advisorVal,volunteer:volunteerVal, bio: clubBio, officers:clubOfficers, website: clubWebsite, code: clubCode}
+	var dataSet = {clubId: document.getElementById("saveButton").value, formData: formdata}
+	$.ajax({
+		url: "queries.php",
+		cache: false,
+		type: "POST",
+		data: {type:"updateClub",parameters:dataSet},
+		success: function(res) {
+			// console.log(res)
+		}
+});
+}
+else{
+console.log("Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+}
+}
+function userJoinClub(clubIdVal){
+	// console.log(clubIdVal)
+	var dataSet = {clubID:clubIdVal, userID:sessionStorage.getItem('userID'), idverify:sessionStorage.getItem('verifyid')}
+	// console.log(dataSet)
+	$.ajax({
+		url: "queries.php",
+		cache: false,
+		type: "POST",
+		data: {type:"userJoinClub",parameters:dataSet},
+		success: function(res) {
+			if(res.substring(res.length-13)=="invalid login"){
+				sessionInvalid()
+			}
+			// console.log(res)
+			getAllClubs()
+		}
+	});
+}
+function userLeaveClub(clubIdVal){
+	// console.log(clubIdVal)
+	var dataSet = {clubID:clubIdVal, userID:sessionStorage.getItem('userID'), idverify:sessionStorage.getItem('verifyid')}
+	// console.log(dataSet)
+	$.ajax({
+		
+		url: "queries.php",
+		cache: false,
+		type: "POST",
+		data: {type:"userLeaveClub",parameters:dataSet},
+		success: function(res) {
+			if(res.substring(res.length-13)=="invalid login"){
+				sessionInvalid()
+			}
+			// console.log(res)
+			userClubs();
+		}
+	});
+}
+function sessionInvalid(){
+	signOut()
+	loggedOut()
+	sessionMessage()
+}
+function sessionMessage(){
+	alert("invalid session")
 }
